14baac13c2773b0010789a158c077c6352bae40f
Jonathan Becker <jonathan@j-be.info>
2023-11-25 11:44:42 +0100

    prettify output

--- scripts/mensa.py
+++ scripts/mensa.py
@@ -5,7 +5,11 @@ import urllib.request as request
 import urllib.error
 from bs4 import BeautifulSoup
 import sys
+from rich.console import Console
+from rich.table import Table
+import re
 
+console = Console()
 diff=0
 if len(sys.argv) > 1 and sys.argv[1].isdigit():
     diff=float(sys.argv[1])
@@ -18,7 +22,9 @@ quote_page = "https://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_
 try:
     page = request.urlopen(quote_page)
 except urllib.error.HTTPError as e:
-    print(f"No menu for {date.strftime('%Y-%m-%d')}")
+    console.print(f"No menu for {date.strftime('%Y-%m-%d')} 🚫📆🍽️ ")
+    console.print(f"\n[i]add number of days to get menu for other days[/i]")
+    console.print(f"[i]e.g.[/i] mensa.py 1")
     sys.exit(1)
 # parse the html using beautiful soup and store in variable soup
 soup = BeautifulSoup(page, "html.parser")
@@ -30,12 +36,21 @@ datum=datum.span.text.strip()
 
 liste = box.find("ul", attrs={"class": "c-schedule__list"})
 items=liste.find_all("li")
-print(datum)
+
+console.rule(datum, style="green")
+table = Table(show_header=True, header_style="bold blue")
+table.add_column("Art")
+table.add_column(f"Gericht")
+
 for item in items:
     artname=item.find("span", attrs={"class": "stwm-artname"}).text.strip()
     if artname == "Beilagen":
-        print("\n"),
+        table.add_section()
     if not artname:
         artname="\t"
-    print(f'{artname}\t', end="")
-    print(item.find("p", attrs={"class": "js-schedule-dish-description"}).text.strip())
+    desc = item.find("p", attrs={"class": "js-schedule-dish-description"}).text
+    desc = desc.replace("[Allergene]","").strip()
+    desc = re.sub(r'\((.*?)\)', r'[green](\1)[/green]', desc)
+    table.add_row(artname, desc)
+
+console.print(table)
